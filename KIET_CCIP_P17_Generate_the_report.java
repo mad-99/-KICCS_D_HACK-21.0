5bc9bcb570625865b8841d3e41f7110b90b26d28
mad-99 <madhurmaheshwari1999@gmail.com>
2021-09-13 22:27:18 +0530

    KDH

--- KIET_CCIP_P17_Generate_the_report.java
+++ KIET_CCIP_P17_Generate_the_report.java
@@ -4,10 +4,10 @@ class Solution {
 
     static String one[] = { "", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine ", "ten ",
             "eleven ", "twelve ", "thirteen ", "fourteen ", "fifteen ", "sixteen ", "seventeen ", "eighteen ",
-            "nineteen " };
+            "nineteen "};
 
     static String ten[] = { "", "", "twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ",
-            "ninety " };
+            "ninety "};
 
     static String numToWords(int n, String s) {
         String str = "";
